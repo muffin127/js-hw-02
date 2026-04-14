b5571e47d6a6e1d9b93c3492117c66992f15f411
Maksim Kozka <kozkamaksim308@gmail.com>
2026-04-15 01:21:02 +0300

    add solution

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+function checkForSpam(message) {
+  const spamLowerCase = message.toLowerCase();
+  const isSpam = spamLowerCase.includes('spam') || spamLowerCase.includes('sale');
+  return isSpam;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
